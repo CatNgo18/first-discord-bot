dd85fd5c6855133daf70cdd5f74ac6b52944afc9
CatNgo18 <cattien.nhu.ngo@gmail.com>
2024-05-18 16:16:27 -0700

    slash commands options and choices

--- src/index.js
+++ src/index.js
@@ -14,6 +14,20 @@ client.on('ready', (c) => {
     console.log(`${c.user.tag} is online.`);
 });
 
+
+// When user (that's not a bot) sends a message that says "hello", bot will reply w/ "hello"
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'hello') {
+        message.reply('hello');
+    }
+});
+
+
+// Slash commands
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
@@ -21,8 +35,11 @@ client.on('interactionCreate', (interaction) => {
         interaction.reply('hey!');
     }
 
-    if (interaction.commandName === 'ping') {
-        interaction.reply('Pong!');
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+
+        interaction.reply(`The sum is ${num1+num2}`)
     }
 });
 
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { REST, Routes } = require('discord.js');
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js');
 
 const commands = [
     {
@@ -7,8 +7,36 @@ const commands = [
         description: 'Replies with hey!',
     },
     {
-        name: 'ping',
-        description: 'Pong!',
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'first-number',
+                description: 'The first number.',
+                type: ApplicationCommandOptionType.Number,
+                choices: [
+                    {
+                        name: 'one',
+                        value: 1,
+                    },
+                    {
+                        name: 'two',
+                        value: 2,
+                    },
+                    {
+                        name: 'three',
+                        value: 3,
+                    },
+                ],
+                required: true,
+            },
+            {
+                name: 'second-number',
+                description: 'The second number.',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            },
+        ]
     },
 ];
 
